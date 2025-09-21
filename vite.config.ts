c0ec9fbdffe6aa02adbdd5a1f4e4532f7d3d193f
Brook1906 <bodasvedang@gmail.com>
2025-09-21 18:58:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: [
+      'a651d2be-fc9e-4704-81f9-d4759e3e9d56-00-215snatbtf75k.riker.replit.dev'
+      ]
     host: "::",
     port: 8080,
   },
